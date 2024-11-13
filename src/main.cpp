015335cce79b59474fe7bec0b92ae009710ee58e
abadiet <titouan.abadie@tabadie.eu>
2024-11-13 17:17:46 +0100

    pure C not working

--- src/main.cpp
+++ src/main.cpp
@@ -12,78 +12,50 @@
 void cb (uvc_frame_t *frame, void *arg) {
     uvc_frame_t *bgr;
     uvc_error_t ret;
-    /* FILE *fp;
-    * static int jpeg_count = 0;
-    * static const char *H264_FILE = "iOSDevLog.h264";
-    * static const char *MJPEG_FILE = ".jpeg";
-    * char filename[16]; */
+    IplImage *img;
 
-    /* We'll convert the image from YUV/JPEG to BGR, so allocate space */
-    bgr = uvc_allocate_frame (frame->width * frame->height * 3);
-    if (!bgr) {
-        printf ("unable to allocate bgr frame!\n");
-        return;
-    }
-
+    /* Print info */
     printf ("callback! frame_format = %d, width = %d, height = %d, length = %lu\n",
         frame->frame_format, frame->width, frame->height, frame->data_bytes);
 
-    switch (frame->frame_format) {
-        case UVC_FRAME_FORMAT_H264:
-            /* use `ffplay H264_FILE` to play */
-            /* fp = fopen(H264_FILE, "a");
-            * fwrite(frame->data, 1, frame->data_bytes, fp);
-            * fclose(fp); */
-            break;
-        case UVC_COLOR_FORMAT_MJPEG:
-            /* sprintf(filename, "%d%s", jpeg_count++, MJPEG_FILE);
-            * fp = fopen(filename, "w");
-            * fwrite(frame->data, 1, frame->data_bytes, fp);
-            * fclose(fp); */
-            break;
-        case UVC_COLOR_FORMAT_YUYV:
-            /* Do the BGR conversion */
-            ret = uvc_any2bgr (frame, bgr);
-            if (ret) {
-                uvc_perror (ret, "uvc_any2bgr");
-                uvc_free_frame (bgr);
-                return;
-            }
-            break;
-        default:
-            break;
+    /* Convert to BGR */
+    if (
+        frame->frame_format == UVC_FRAME_FORMAT_YUYV ||
+        frame->frame_format == UVC_FRAME_FORMAT_UYVY
+    ) {
+        bgr = uvc_allocate_frame (frame->width * frame->height * 3);
+        if (!bgr) {
+            printf ("unable to allocate bgr frame!\n");
+            return;
+        }
+
+        ret = uvc_any2bgr (frame, bgr);
+        if (ret) {
+            uvc_perror (ret, "uvc_any2bgr");
+            uvc_free_frame (bgr);
+            return;
+        }
+
+        ret = uvc_duplicate_frame (bgr, frame);
+        if (ret) {
+            uvc_perror (ret, "uvc_duplicate_frame");
+            uvc_free_frame (bgr);
+            return;
+        }
+
+        uvc_free_frame (bgr);
     }
 
-    /* Call a user function:
-    *
-    * my_type *my_obj = (*my_type) ptr;
-    * my_user_function(ptr, bgr);
-    * my_other_function(ptr, bgr->data, bgr->width, bgr->height);
-    */
-
-    /* Call a C++ method:
-    *
-    * my_type *my_obj = (*my_type) ptr;
-    * my_obj->my_func(bgr);
-    */
-
-    /* Use opencv.highgui to display the image:
-    * 
-    * cvImg = cvCreateImageHeader(
-    *     cvSize(bgr->width, bgr->height),
-    *     IPL_DEPTH_8U,
-    *     3);
-    *
-    * cvSetData(cvImg, bgr->data, bgr->width * 3); 
-    *
-    * cvNamedWindow("Test", CV_WINDOW_AUTOSIZE);
-    * cvShowImage("Test", cvImg);
-    * cvWaitKey(10);
-    *
-    * cvReleaseImageHeader(&cvImg);
-    */
-
-    uvc_free_frame (bgr);
+    img = cvCreateImageHeader(
+        cvSize(frame->width, frame->height),
+        IPL_DEPTH_8U,
+        3
+    );
+    cvSetData(img, frame->data, frame->width * 3); 
+    cvNamedWindow("Test", CV_WINDOW_AUTOSIZE);
+    cvShowImage("Test", img);
+    cvWaitKey(10);
+    cvReleaseImageHeader(&img);
 
     UNUSED (arg);
 }
@@ -122,61 +94,70 @@ int main (int argc, char **argv) {
             /* Print out a message containing all the information that libuvc knows about the device */
             uvc_print_diag (devh, stderr);
 
-            const uvc_format_desc_t *format_desc = uvc_get_format_descs (devh);
-            const uvc_frame_desc_t *frame_desc = format_desc->frame_descs;
-            enum uvc_frame_format frame_format;
-            /* Default values */
-            int width = 640;
-            int height = 480;
-            int fps = 30;
-
-            switch (format_desc->bDescriptorSubtype) {
-                case UVC_VS_FORMAT_MJPEG:
-                    frame_format = UVC_COLOR_FORMAT_MJPEG;
-                    break;
-                case UVC_VS_FORMAT_FRAME_BASED:
-                    frame_format = UVC_FRAME_FORMAT_H264;
-                    break;
-                default:
-                    frame_format = UVC_FRAME_FORMAT_YUYV;
-                    break;
+            const uvc_format_desc_t * format_desc = uvc_get_format_descs (devh);
+            while (format_desc && format_desc->bDescriptorSubtype == UVC_VS_FORMAT_MJPEG) {
+                format_desc = format_desc->next;
             }
-
-            if (frame_desc) {
-                width = frame_desc->wWidth;
-                height = frame_desc->wHeight;
-                fps = 10000000 / frame_desc->dwDefaultFrameInterval;
-            }
-
-            printf("\nFirst format: (%4s) %dx%d %dfps\n", format_desc->fourccFormat, width, height, fps);
-
-            /* Try to negotiate first stream profile */
-            res = uvc_get_stream_ctrl_format_size (
-                devh, &ctrl, /* result stored in ctrl */
-                frame_format,
-                width, height, fps
-            );
-
-            /* Print out the result */
-            uvc_print_stream_ctrl (&ctrl, stderr);
-
-            if (res < 0) {
-                uvc_perror (res, "get_mode"); /* device doesn't provide a matching stream */
+            if (!format_desc) {
+                puts ("No non-MJPEG formats found");
             } else {
-                res = uvc_start_streaming (devh, &ctrl, cb, NULL, 0);
+
+                const uvc_frame_desc_t *frame_desc = format_desc->frame_descs;
+                enum uvc_frame_format frame_format;
+                /* Default values */
+                int width = 640;
+                int height = 480;
+                int fps = 30;
+
+                switch (format_desc->bDescriptorSubtype) {
+                    case UVC_VS_FORMAT_MJPEG:
+                        /* Impossible */
+                        frame_format = UVC_COLOR_FORMAT_MJPEG;
+                        break;
+                    case UVC_VS_FORMAT_FRAME_BASED:
+                        frame_format = UVC_FRAME_FORMAT_H264;
+                        break;
+                    default:
+                        frame_format = UVC_FRAME_FORMAT_YUYV;
+                        break;
+                }
+
+                if (frame_desc) {
+                    width = frame_desc->wWidth;
+                    height = frame_desc->wHeight;
+                    fps = 10000000 / frame_desc->dwDefaultFrameInterval;
+                }
+
+                printf("\nFirst non-MJPEG format: (%4s) %dx%d %dfps\n", format_desc->fourccFormat, width, height, fps);
+
+                /* Try to negotiate first stream profile */
+                res = uvc_get_stream_ctrl_format_size (
+                    devh, &ctrl, /* result stored in ctrl */
+                    frame_format,
+                    width, height, fps
+                );
+
+                /* Print out the result */
+                uvc_print_stream_ctrl (&ctrl, stderr);
 
                 if (res < 0) {
-                    uvc_perror (res, "start_streaming"); /* unable to start stream */
+                    uvc_perror (res, "get_mode"); /* device doesn't provide a matching stream */
                 } else {
-                    puts ("Streaming...");
+                    res = uvc_start_streaming (devh, &ctrl, cb, NULL, 0);
 
-                    uvc_set_ae_mode (devh, 1); /* e.g., turn on auto exposure */
+                    if (res < 0) {
+                        uvc_perror (res, "start_streaming"); /* unable to start stream */
+                    } else {
+                        puts ("Streaming...");
 
-                    sleep (10); /* stream for 10 seconds */
+                        uvc_set_ae_mode (devh, 1); /* e.g., turn on auto exposure */
 
-                    /* End the stream. Blocks until last callback is serviced */
-                    uvc_stop_streaming (devh);
-                    puts ("Done streaming.");
+                        sleep (3); /* stream for x seconds */
+
+                        /* End the stream. Blocks until last callback is serviced */
+                        uvc_stop_streaming (devh);
+                        puts ("Done streaming.");
+                    }
                 }
             }
 
