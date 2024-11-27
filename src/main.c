fbceda4e058b2006a503f862027861d916292379
abadiet <titouan.abadie@tabadie.eu>
2024-11-27 15:50:07 +0100

    uvc_open not working

--- src/main.c
+++ src/main.c
@@ -1,47 +1,74 @@
-#include <thetauvc.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <libuvc/libuvc.h>
+#include "thetauvc.h"
 
 
 #define UNUSED(x) (void)(x)
 
-int main (int argc, char **argv) {
+void callback(uvc_frame_t *frame, void *ptr){
+
+    printf("Frame %u: %lu bytes\n", frame->sequence, frame->data_bytes);
+
+    UNUSED (ptr);
+	return;
+}
+
+int main(int argc, char **argv){
     uvc_context_t *ctx;
-	uvc_device_t **devlist;
+	uvc_device_t *dev;
+	uvc_device_handle_t *devh;
+    uvc_device_descriptor_t *desc;
+	uvc_stream_ctrl_t ctrl;
 	uvc_error_t res;
-    int idx = 0;
 
 	res = uvc_init(&ctx, NULL);
 	if (res != UVC_SUCCESS) {
 		uvc_perror(res, "uvc_init");
-		return res;
+		return 1;
 	}
 
-    res = thetauvc_find_devices(ctx, &devlist);
-    if (res != UVC_SUCCESS) {
-        uvc_perror(res,"");
-        uvc_exit(ctx);
-        return res;
+    if (argc > 1 && strcmp("-l", argv[1]) == 0) {
+		thetauvc_print_devices(ctx, stdout);
+		uvc_exit(ctx);
+	    return 0;
+	}
+
+	res = thetauvc_find_device(ctx, &dev, 0);
+	if (res != UVC_SUCCESS) {
+		uvc_perror(res, "Device not found");
+		uvc_exit(ctx);
+	    return 1;
+	}
+
+    if (uvc_get_device_descriptor(dev, &desc) == UVC_SUCCESS) {
+        printf("Using %s (SN:%s)\n", desc->product, desc->serialNumber);
+    } else {
+        printf ("Failed to get device descriptor\n");
     }
+    uvc_free_device_descriptor(desc);
 
-    printf ("Devices found:\n");
-	printf("# No : %-18s : %-10s\n", "Product", "Serial");
-    while (devlist[idx] != NULL) {
-        uvc_device_descriptor_t *desc;
+	res = uvc_open(dev, &devh);
+	if (res != UVC_SUCCESS) {
+		uvc_perror(res, "Can't open the device");
+		uvc_exit(ctx);
+	    return 1;
+	}
+	
+	res = thetauvc_get_stream_ctrl_format_size(devh, THETAUVC_MODE_UHD_2997, &ctrl);
 
-        if (uvc_get_device_descriptor(devlist[idx], &desc) != UVC_SUCCESS)
-            continue;
+	res = uvc_start_streaming(devh, &ctrl, callback, NULL, 0);
+	if (res == UVC_SUCCESS) {
+		printf("Stream has begun, hit any key to stop it...\n");
 
-        printf("# %2d : %-18s : %-10s\n", idx, desc->product,
-            desc->serialNumber);
+		printf("Stream interrupted\n");
 
-        uvc_free_device_descriptor(desc);
-        idx++;
-    }
+		uvc_stop_streaming(devh);
+	}
 
-    uvc_free_device_list(devlist, 1);
-    uvc_exit(ctx);
+	uvc_close(devh);
+	uvc_exit(ctx);
 
-    UNUSED (argc);
-    UNUSED (argv);
+    return 0;
 }
